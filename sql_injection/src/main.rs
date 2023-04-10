2b656efb18e2f033af2c013abe098304ac77e9b4
Sathwik Amburi <sathwik.amburi@gmail.com>
2023-04-10 21:06:56 +0200

    sql-injection attack simulation completed

--- sql_injection/src/main.rs
+++ sql_injection/src/main.rs
@@ -30,10 +30,27 @@ fn insert_dummy_users(conn: &Connection) -> Result<()> {
     Ok(())
 }
 
-fn get_users_by_name(conn: &Connection, name: &str) -> Result<Vec<User>> {
-    let query = format!("SELECT id, name, age FROM users WHERE name = '{}'", name);
-    let mut stmt = conn.prepare(&query)?;
-    let user_iter = stmt.query_map([], |row| {
+// fn get_users_by_name(conn: &Connection, name: &str) -> Result<Vec<User>> {
+//     let query = format!("SELECT id, name, age FROM users WHERE name = '{}'", name);
+//     let mut stmt = conn.prepare(&query)?;
+//     let user_iter = stmt.query_map([], |row| {
+//         Ok(User {
+//             id: row.get(0)?,
+//             name: row.get(1)?,
+//             age: row.get(2)?,
+//         })
+//     })?;
+//     let mut users = vec![];
+//     for user in user_iter {
+//         users.push(user?);
+//     }
+//     Ok(users)
+// }
+
+fn get_users_by_name_safe(conn: &Connection, name: &str) -> Result<Vec<User>> {
+    let query = "SELECT id, name, age FROM users WHERE name = ?";
+    let mut stmt = conn.prepare(query)?;
+    let user_iter = stmt.query_map(&[name], |row| {
         Ok(User {
             id: row.get(0)?,
             name: row.get(1)?,
@@ -53,10 +70,15 @@ fn main() -> Result<()> {
     insert_dummy_users(&conn)?;
 
     // Simulate user input
-    let name = "Charlie' OR 1=1; --";
+    let name = "Charlie";
+    //SQL injection
+    // let name = "Charlie' OR 1=1; --";
 
     // Demonstrate SQL injection vulnerability
-    let users = get_users_by_name(&conn, name)?;
+    // let users = get_users_by_name(&conn, name)?;
+
+    let users = get_users_by_name_safe(&conn, name)?;
+
     println!("{:?}", users);
 
     Ok(())
