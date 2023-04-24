58de013431e1f73297a159f94546a0d701658e5e
Sathwik Amburi <sathwik.amburi@gmail.com>
2023-04-24 11:01:07 +0200

    if you use process::Command use shellescape

--- command_injection/src/main.rs
+++ command_injection/src/main.rs
@@ -13,6 +13,24 @@
 //     println!("{}", String::from_utf8_lossy(&output.stdout));
 // }
 
+// use std::env;
+// use std::process::Command;
+// use shell_escape::escape;
+
+// fn main() {
+//     let args: Vec<String> = env::args().collect();
+//     let file_name = &args[1];
+
+//     let escaped_file_name = escape(file_name);
+
+//     let output = Command::new("ls")
+//         .arg(&escaped_file_name)
+//         .output()
+//         .expect("Failed to execute command");
+
+//     println!("{}", String::from_utf8_lossy(&output.stdout));
+// }
+
 use std::env;
 use std::fs;
 use std::io::ErrorKind;
