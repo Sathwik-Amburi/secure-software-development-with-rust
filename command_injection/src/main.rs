24eeb98fa9f6bdf5c37a51f931b4b43a25047941
Sathwik Amburi <sathwik.amburi@gmail.com>
2023-04-24 10:59:34 +0200

    safer to use std library functions

--- command_injection/src/main.rs
+++ command_injection/src/main.rs
@@ -1,14 +1,53 @@
+// use std::env;
+// use std::process::Command;
+
+// fn main() {
+//     let args: Vec<String> = env::args().collect();
+//     let file_name = &args[1];
+
+//     let output = Command::new("ls")
+//         .arg(file_name)
+//         .output()
+//         .expect("Failed to execute command");
+
+//     println!("{}", String::from_utf8_lossy(&output.stdout));
+// }
+
 use std::env;
-use std::process::Command;
+use std::fs;
+use std::io::ErrorKind;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let file_name = &args[1];
+    if args.len() < 2 {
+        println!("Usage: {} <directory>", args[0]);
+        return;
+    }
 
-    let output = Command::new("ls")
-        .arg(file_name)
-        .output()
-        .expect("Failed to execute command");
+    let dir_name = &args[1];
 
-    println!("{}", String::from_utf8_lossy(&output.stdout));
+    match fs::read_dir(dir_name) {
+        Ok(entries) => {
+            for entry_result in entries {
+                match entry_result {
+                    Ok(entry) => {
+                        let entry_path = entry.path();
+                        if let Some(file_name) = entry_path.file_name() {
+                            println!("{}", file_name.to_string_lossy());
+                        }
+                    }
+                    Err(e) => {
+                        eprintln!("Error: {}", e);
+                    }
+                }
+            }
+        }
+        Err(e) => {
+            if e.kind() == ErrorKind::NotFound {
+                eprintln!("Error: Directory not found");
+            } else {
+                eprintln!("Error: {}", e);
+            }
+        }
+    }
 }
