de0e5561c0dda45c852f8ef4363b12464fbcf36c
Sathwik Amburi <sathwik.amburi@gmail.com>
2023-05-01 17:09:37 +0200

    integer overflow

--- integer_overflow/src/main.rs
+++ integer_overflow/src/main.rs
@@ -4,22 +4,25 @@ fn main() {
     let mut balance = 100.0; // initial balance
     println!("Current balance: ${}", balance);
 
-    // Prompt user for withdrawal amount
-    println!("Enter withdrawal amount: ");
-    let mut withdrawal_amount = String::new();
-    io::stdin()
-        .read_line(&mut withdrawal_amount)
-        .expect("Failed to read line");
+    loop {
+        // Prompt user for withdrawal amount
+        println!("Enter withdrawal amount: ");
+        let mut withdrawal_amount = String::new();
+        io::stdin()
+            .read_line(&mut withdrawal_amount)
+            .expect("Failed to read line");
 
-    // Parse withdrawal amount as a float
-    let withdrawal_amount: f64 = withdrawal_amount.trim().parse().expect("Invalid input");
+        // Parse withdrawal amount as a float
+        let withdrawal_amount: f64 = withdrawal_amount.trim().parse().expect("Invalid input");
 
-    // Check if withdrawal amount is greater than balance
-    if withdrawal_amount > balance {
-        panic!("Insufficient funds");
+        // Check if withdrawal amount is greater than balance
+        if withdrawal_amount > balance {
+            println!("Insufficient funds");
+            continue;
+        }
+
+        // Update account balance
+        balance -= withdrawal_amount;
+        println!("Withdrawal successful. New balance: ${}", balance);
     }
-
-    // Update account balance
-    balance -= withdrawal_amount;
-    println!("Withdrawal successful. New balance: ${}", balance);
 }
