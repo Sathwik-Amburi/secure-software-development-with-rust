bd2faafe0ace892c20fe2611d3f06efb4521dab1
Sathwik Amburi <sathwik.amburi@gmail.com>
2023-05-23 11:00:27 +0200

    changes to command injection

--- command_injection/src/secure.rs
+++ command_injection/src/secure.rs
@@ -1,11 +1,7 @@
 use std::io;
 use std::process::Command;
-mod shared;
 
 fn main() {
-    let script_path = "./custom_commands/myutility.sh";
-    shared::make_script_executable(&script_path);
-
     println!("Enter a command to manage your lights (on/off/dim):");
     let mut input = String::new();
     io::stdin()
@@ -14,8 +10,7 @@ fn main() {
     let user_input = input.trim();
 
     let output = Command::new("sh")
-        .arg("-c")
-        .arg(script_path)
+        .arg("./custom_commands/myutility.sh")
         .arg(user_input)
         .output()
         .expect("Failed to execute command");
--- command_injection/src/vulnerable.rs
+++ command_injection/src/vulnerable.rs
@@ -1,11 +1,7 @@
 use std::io;
 use std::process::Command;
-mod shared;
 
 fn main() {
-    let script_path = "./custom_commands/myutility.sh";
-    shared::make_script_executable(&script_path);
-
     println!("Enter a command to manage your lights (on/off/dim):");
     let mut input = String::new();
     io::stdin()
@@ -13,7 +9,7 @@ fn main() {
         .expect("Failed to read input");
     let user_input = input.trim();
 
-    let command = format!("{} {}", script_path, user_input);
+    let command = format!("./custom_commands/myutility.sh {}", user_input);
     let output = Command::new("sh")
         .arg("-c")
         .arg(command)
